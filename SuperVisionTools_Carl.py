77809150f7ed3907633c6d5b09cfdd2ac9e2c010
Carl <carlche@bu.edu>
2016-09-20 20:53:33 -0400

    Update SuperVisionTools_Carl.py

--- SuperVisionTools_Carl.py
+++ SuperVisionTools_Carl.py
@@ -40,19 +40,21 @@ class Chart_Factory:
                       self.subline_rsi={}
                       self.subline_macd={}
                       self.subline_obv={}
+                      self.subline_volume={}
                       self.current_indicator=0
-                      self.figure_map={"candle":False,"price":False,"rsi":False,"macd":False,"obv":False,"portfolio":False}
+                      self.figure_map={"candle":False,"price":False,"rsi":False,"macd":False,"obv":False,"portfolio":False,"volume":False}
 
  # This is a cheap shot, has to be mended later on~~~~~
          def add_index(self,stockticker):
                     self.index=stockticker.price()[1].index
 
-         def push_data(self,method,isprice=False,iscandle=False,issubline_rsi=False,issubline_macd=False,issubline_obv=False,isportolio=False):
+         def push_data(self,method,isprice=False,iscandle=False,issubline_rsi=False,issubline_macd=False,issubline_obv=False,isportolio=False, issubline_volume=False):
 
                     if (iscandle):
                              index, value = method
                              self.candleprice_data[index] = value
                              self.figure_map["candle"]=True
+
                     elif (isprice):
                              index, value = method
                              self.price_data[index] = value
@@ -84,6 +86,15 @@ class Chart_Factory:
                                          self.subline_obv[index] = value
                              self.figure_map["obv"] = True
 
+                    elif (issubline_volume):
+                        self.indicator += 1
+                        for i in range(0, len(method), 2):
+                            index = method[i]
+                            value = method[1 + i]
+                            self.subline_volume[index] = value
+                        self.figure_map["volume"] = True
+
+
                     else:
                                  for i in range(0,len(method),2):
                                                 index=method[i]
@@ -99,12 +110,14 @@ class Chart_Factory:
                                   formatter = MyFormatter(index)  #
 
                                   ax.xaxis.set_major_formatter(formatter)  #
-                                  ax.plot(ind, data)  #
+
+                                  ax.plot(ind, data, lw=3, c="skyblue")  #
                                   formatter = ticker.FormatStrFormatter('$%1.2f')
                                   ax.yaxis.set_major_formatter(formatter)
                                   min_temp = np.min(data)
                                   max_temp=np.max(data)
                                   plt.xticks()
+                                  ax.set_title("Price",fontsize=12)
 
           ######designed for equally displaying codes!!!!!!!
                                   date_min = np.min(ind)
@@ -127,16 +140,15 @@ class Chart_Factory:
                                   current_price="Current: "+current_price
                                   open_price="Open: "+open_price
                                   high_price="High: "+high_price
-                                  print high_price
                                   low_price="Low: "+low_price
                                   Last_price="Last: "+Last_price
                                   change_price="Chg: "+change_price
                                   volume="Volume: "+volume
                                   ss=current_time+'    '+ current_price+"    "+open_price+"    "+high_price+"    "+low_price+"    "+Last_price+"    "+volume+"    "+change_price
+                                  plt.text(date_min+2,max_temp+8,ss,fontsize=15)
 
-                                  plt.text(date_min+2,max_temp+12,ss,fontsize=15)
+                                  ax.fill_between(ind, min_temp - 10.0, data, color="dodgerblue",alpha=0.4)
 
-                                 # ax.fill_between(ind, min_temp - 10.0, data, color="lightsteelblue")
                                   cursor1 = Cursor_haunter(ax, ind, data, "Close price", 1)
 
 
@@ -149,7 +161,7 @@ class Chart_Factory:
                                 ind = np.arange(len(index))  #
                                 formatter = MyFormatter(index)  #
                                 ax.xaxis.set_major_formatter(formatter)  #
-                                ax.plot(ind, data)  #
+                                ax.plot(ind, data,alpha=0.8)  #
           # fig.autofmt_xdate()  #
                                 date_min = np.min(ind)
                                 date_max = np.max(ind)
@@ -172,7 +184,8 @@ class Chart_Factory:
                                      candle(ax, list, width=0.6)
                      plt.title("", fontsize=20)
                      plt.ylabel("Price/USD", fontsize=14)
-     # plt.xlabel("Dates", fontsize=14)
+
+
 
          def plot_sub_rsi(self,ax,fig,gs):
 
@@ -188,6 +201,7 @@ class Chart_Factory:
                                                 formatter = MyFormatter(index)  #
                                                 ax.xaxis.set_major_formatter(formatter)  #
                                                 ax.plot(ind, data, color="tomato")  #
+                                                ax.set_title("Relative Strength Index", fontsize=12)
                                                 # fig.autofmt_xdate()  #
 
                                                 date_min = np.min(ind)
@@ -210,15 +224,15 @@ class Chart_Factory:
                                                       ind = np.arange(len(index))  #
                                                       formatter = MyFormatter(index)  #
                                                       ax.xaxis.set_major_formatter(formatter)  #
-                                                      ax.plot(ind, data,)  #
+                                                      ax.plot(ind, data,alpha=0.8)  #
                                                       # fig.autofmt_xdate()  #
                                                       date_min = np.min(ind)
                                                       date_max = np.max(ind)
                                                       plt.xlim([date_min, date_max])
-
+                                 ax.set_title("Moving Average Convergence Divergence Line", fontsize=12)
                                  index=self.subline_macd[sig_temp[0]].index
                                  data=self.subline_macd[sig_temp[0]].values
-                                 ax.bar(ind, data,color="teal")
+                                 ax.bar(ind, data,color="teal",alpha=0.8)
                                  # cursor2 = Cursor_haunter(ax, index, data, "MACD Signal:", 1)
                                  # the above line of code is remained to be seen sicne it is far too slow to run!!!!
                                  plt.legend(self.subline_macd.keys(), fontsize=12)
@@ -239,12 +253,12 @@ class Chart_Factory:
                                                       ind = np.arange(len(index))  #
                                                       formatter = MyFormatter(index)  #
                                                       ax.xaxis.set_major_formatter(formatter)  #
-                                                      ax.bar(ind, data, color=color_map[temp_i])  #
+                                                      ax.bar(ind, data, color=color_map[temp_i],alpha=0.7)  #
                                                       temp_i += 1
                                                       date_min = np.min(ind)
                                                       date_max = np.max(ind)
                                                       plt.xlim([date_min, date_max])
-
+                            ax.set_title("On Balance Volume", fontsize=12)
                             index = self.subline_obv[sig_temp[0]].index
                             data = self.subline_obv[sig_temp[0]].values
                             ind = np.arange(len(index))  #
@@ -256,13 +270,45 @@ class Chart_Factory:
                             plt.legend(["OBV", "Trading volume"], fontsize=12)
 
 
+         def plot_sub_volume(self, ax, fig, gs):
+                if (len(self.subline_volume) != 0):
+
+                          sig_temp = [x for x in self.subline_volume.keys() if "balance" in x]
+                          color_map = ["r", "g"]
+                          temp_i = 0
+                          plt.title(str(self.subline_volume.keys()[0]), fontsize=13)
+                          for i in self.subline_volume:
+                                    if i not in sig_temp:
+                                           index = self.subline_volume[i].index
+                                           data = self.subline_volume[i].values
+                                           ind = np.arange(len(index))  #
+                                           formatter = MyFormatter(index)  #
+                                           ax.xaxis.set_major_formatter(formatter)  #
+                                           ax.bar(ind, data, color=color_map[temp_i], alpha=0.7)  #
+                                           temp_i += 1
+                                           date_min = np.min(ind)
+                                           date_max = np.max(ind)
+                                           plt.xlim([date_min, date_max])
+                          ax.set_title("Trading Volume",fontsize=12)
+                          index = self.subline_volume[sig_temp[0]].index
+                          data = self.subline_volume[sig_temp[0]].values
+                          ind = np.arange(len(index))  #
+                          formatter = MyFormatter(index)  #
+                          ax.xaxis.set_major_formatter(formatter)  #
+                          # ax.plot(ind, data)  #
+                          # cursor2 = Cursor_haunter(ax, ind, data, "On Balance Volume:", 1)
+        # fig.autofmt_xdate()  #
+
+
+
          def plot_all(self):
                   fig=plt.figure()
                   plot_map_1=[1.8]
                   plot_map_2=[1.8,1]
                   plot_map_3 = [1.8,1,1]
                   plot_map_4 = [1.8,1,1,1]
-                  plot_map=[plot_map_1,plot_map_2,plot_map_3,plot_map_4]
+                  plot_map_5=[1.8,1,1,1,1]
+                  plot_map=[plot_map_1,plot_map_2,plot_map_3,plot_map_4,plot_map_5]
                   gs = gridspec.GridSpec(self.indicator+1, 1, height_ratios=plot_map[self.indicator])
                   ax = fig.add_subplot(gs[self.current_indicator])
                   # ax.set_axis_bgcolor('darkslategray')
@@ -283,6 +329,12 @@ class Chart_Factory:
                                  self.current_indicator +=1
                                  ax = fig.add_subplot(gs[self.current_indicator], sharex=ax)
                                  self.plot_sub_obv(ax,fig,gs)
+
+                  if (self.figure_map["volume"]):
+
+                      self.current_indicator += 1
+                      ax = fig.add_subplot(gs[self.current_indicator], sharex=ax)
+                      self.plot_sub_volume(ax, fig, gs)
                   plt.show()
 class Chart_Factory_Portfolio:
   #global indicator
@@ -290,9 +342,9 @@ class Chart_Factory_Portfolio:
   def __init__(self):
       self.portfolio={}
       self.current_indicator=0
-      self.figure_map={"portfolio":False,"weights":False,"risk":False}
+      self.figure_map={"portfolio":False,"weights":False,"risk":False,"pnl":False}
 
-  def push_data(self,method,isportfolio=False,isweight=False,isrisk=False):
+  def push_data(self,method,isportfolio=False,isweight=False,isrisk=False,ispnl=False):
               if(isportfolio):
                          index,value=method
                          self.portfolio[index]=value
@@ -303,6 +355,12 @@ class Chart_Factory_Portfolio:
                          index,value=method
                          self.portfolio[index]=value
                          self.figure_map["weights"]=True
+
+              if(ispnl):
+                         self.indicator2+=1
+                         index, value=method
+                         self.portfolio[index]=value
+                         self.figure_map["pnl"]=True
 
               if(isrisk):
                          self.indicator2+=1
@@ -319,10 +377,9 @@ class Chart_Factory_Portfolio:
                         z_str_temp="z"+str(i)
                         # color=np.random.rand(len(df[x_str_temp]))
                         color=["red","green","blue","cyan","orange","grey","purple","darkgreen","gold","forestgreen","lightcyan","violet"]
-                        print df[z_str_temp]
                         area=np.pi * (600000* df[z_str_temp].values) ** 2
                         plt.scatter(x=df[x_str_temp].values, y=df[y_str_temp].values, s=area, c=color, edgecolors=None, alpha=0.4)
-            plt.title("Passive Portfolio Management Risk Distribution",fontsize=18)
+            plt.title("Current Portfolio Management Risk Distribution",fontsize=18)
             tickers=self.portfolio["weights"].index
             plt.xticks(np.arange(len(tickers)), tickers)
             plt.yticks(np.arange(len(tickers)), tickers)
@@ -334,9 +391,8 @@ class Chart_Factory_Portfolio:
           data = self.portfolio["Portfolio Value: "].values
           ind = np.arange(len(index))  #
           formatter = MyFormatter(index)  #
-
           ax.xaxis.set_major_formatter(formatter)  #
-          ax.plot(ind, data,color="orange")  #
+          ax.plot(ind, data,color="dodgerblue",lw=2)  #
           formatter = ticker.FormatStrFormatter('$%1.2f')
           ax.yaxis.set_major_formatter(formatter)
           min_temp = np.min(data)
@@ -349,8 +405,10 @@ class Chart_Factory_Portfolio:
 
           ######designed for equally display codes!!!!!!
 
-          plt.title("Passive Portfolio Managment Result",fontsize=18)
-          ax.fill_between(ind,0, data, color="navajowhite")
+          ax.set_title("Portfolio Capital",fontsize=16)
+          ax.set_xlabel("Date", fontsize=10)
+          ax.set_ylabel("Portfolio capital / USD", fontsize=10)
+          ax.fill_between(ind,0, data, color="dodgerblue",alpha=0.45)
           # fig.autofmt_xdate()
           # cursor1 = Cursor_haunter(ax, ind, data, "Portfolio Value", 1)
 
@@ -361,24 +419,52 @@ class Chart_Factory_Portfolio:
                data=self.portfolio["weights"].values
                fake=np.arange(len(index))
                plt.xticks(fake,index)
-               plt.bar(fake,data,color=["limegreen","deepskyblue","plum","darkorange","tomato","y"])
+               ax.bar(fake,data,color=["limegreen","deepskyblue","plum","darkorange","tomato","y"],alpha=0.7)
+               ax.set_title("Final Positions", fontsize=16)
+               ax.set_xlabel("Stock symbols", fontsize=10)
+               ax.set_ylabel("Volume/Share", fontsize=10)
+
                # xxx=self.portfolio["weights"]
                # xxx.plot(kind="bar",color="salmon",colour=["limegreen","deepskyblue","plum","darkslategray","r"])
                # ax.bar(index, data, color="green")  #
                # plt.legend(["OBV", "Trading volume"], fontsize=12)
 
+  def plot_pnl(self, ax):
+              ax = ax
+              index = self.portfolio["Portfolio PnL Graph: "].index
+              data = self.portfolio["Portfolio PnL Graph: "].values
+              ind = np.arange(len(index))
+              formatter=MyFormatter(index)
+              ax.xaxis.set_major_formatter(formatter)
+              ax.plot(ind,data,color="green",lw=1.44,alpha=0.9)
+              ax.set_title("Profit and Loss Graph", fontsize=16)
+              formatter=ticker.FormatStrFormatter("$%1.2f")
+              ax.yaxis.set_major_formatter(formatter)
+              ax.set_xlabel("Date", fontsize=10)
+              ax.set_ylabel("Profit & Loss / USD", fontsize=10)
+              # xxx=self.portfolio["weights"]
+              # xxx.plot(kind="bar",color="salmon",colour=["limegreen","deepskyblue","plum","darkslategray","r"])
+              # ax.bar(index, data, color="green")  #
+              # plt.legend(["OBV", "Trading volume"], fontsize=12)
+
   def plot_all(self):
 
-      plot_map_1=[3]
-      plot_map_2=[3,1]
-      plot_map_3 = [3,1,1]
-      plot_map_4 = [3,1,1,1]
+      plot_map_1=[2]
+      plot_map_2=[2,1]
+      plot_map_3 = [2,1,1]
+      plot_map_4 = [2,1,1,1]
       plot_map=[plot_map_1,plot_map_2,plot_map_3,plot_map_4]
       fig = plt.figure()
-      gs = gridspec.GridSpec(self.indicator2+1, 1, height_ratios=plot_map[self.indicator2])
+      gs = gridspec.GridSpec(self.indicator2, 1, height_ratios=plot_map[self.indicator2-1])
       ax = fig.add_subplot(gs[self.current_indicator])
       # ax.set_axis_bgcolor('darkslategray')
       self.plot_portfolio_val(ax)
+
+      if(self.figure_map["pnl"]):
+                     self.current_indicator+=1
+                     ax=fig.add_subplot(gs[self.current_indicator])
+                     self.plot_pnl(ax)
+
       if (self.figure_map["weights"]):
                      self.current_indicator += 1
                      ax = fig.add_subplot(gs[self.current_indicator])
@@ -389,5 +475,7 @@ class Chart_Factory_Portfolio:
                       self.current_indicator += 1
                       ax=plt.subplot()
                       self.plot_portfolio_risk(ax)
+
+
       plt.show()
       sns.set_style("whitegrid")
